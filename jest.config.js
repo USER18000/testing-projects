d7f0337a4f7b6a054190075bde9ef972f5c71203
Max Mironov <mexc35633@gmail.com>
2025-11-27 03:09:17 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 // jest.config.js
 
 module.exports = {
-  // testRunner: 'jest-circus/runner', // Обычно необязательно, если используется стандартное окружение или кастомное, которое его поддерживает
-  testEnvironment: 'allure-jest/node', // <--- Правильно: строка, не массив
+  // testRunner: 'jest-circus/runner',
+  testEnvironment: 'allure-jest/node', 
   testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).[tj]s?(x)'],
   cache: false,
   verbose: true,
